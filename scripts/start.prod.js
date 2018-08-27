d3bad1f1fdc26a88521a421da41bcaedf77ad32b
zerofruit <14wnrkim@gmail.com>
2018-08-27 23:21:00 +0900

    change index.html title

--- scripts/start.prod.js
+++ scripts/start.prod.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/*", () => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
